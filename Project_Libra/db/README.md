2ba8c699e3c2c0a2389120a3b2987470853515bc
ksmsky8385 <ksmsky8385@naver.com>
2025-07-23 12:56:00 +0900

    Add Project_Libra as submodule

--- Project_Libra/db/README.md
+++
@@ -1,2 +0,0 @@
-sqlplus / as sysdba @libra_setup.sql
-sqlplus / as sysdba @D:\Project_Libra\scripts\libra_setup.sql
--- Project_Libra/model/ml_pipline/ModelCreator/Utiles/Exporter.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-def save_model(model, path: str):
-    try:
-        joblib.dump(model, path)
-        print(f"[저장 완료] 모델 → {path}")
-    except Exception as e:
-        raise RuntimeError(f"[ERROR] 모델 저장 실패 → {e}")
